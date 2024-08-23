7f93ef6ca780a88de71eaaedc8b5358d0777a740
Sanchit24s <sanchit24bhoyar@gmail.com>
2024-08-23 17:14:11 +0530

    Add initial files

---
+++ auth_micro/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+    return res.json({ message: "It's working ..." });
+});
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
